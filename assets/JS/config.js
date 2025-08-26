b94368bb26bb8862793018fdcc4e3a0bb6489a96
Lindrich <djankoufamily@gmail.com>
2025-08-26 19:15:21 +0200

    Update config.js

--- assets/JS/config.js
+++ assets/JS/config.js
@@ -1,5 +1,5 @@
 window.CONFIG = {
-  ENV: "TEST",
+  ENV: "PROD",
   API_URL: "https://script.google.com/macros/s/AKfycbxwbDIA6vgkH7i5Ml7CfKHWtP2a4I967wrjAqq66TxcHL9wuKfPugLfaviUWjNcZZ2o/exec",
   CLOUDINARY_CLOUD_NAME: "dk0ioppgv",
   CLOUDINARY_UPLOAD_PRESET: "Cake_Test",
